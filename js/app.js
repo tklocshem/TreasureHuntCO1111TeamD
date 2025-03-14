c39f56685cda03d741e0f4bd0fca22c6c6212595
71nno <timofey.timofei@gmail.com>
2025-03-14 22:16:51 +0200

    Add form submission handling and "Remember me" functionality
    - Added action to the login form to call `getinfo` function.
    - Integrated `Rememberme` function to handle the "Remember me" checkbox.
    - Implemented cookie management functions (`setCookie`, `getCookie`, `deleteCookie`).

--- js/app.js
+++ js/app.js
@@ -1,20 +1,58 @@
 /<< TO ADD NICK&EMAIL >>
 
-function getinfo(id) {
+function getinfo() {
     const playerNameField = document.getElementById("fname");
-    if(playerNameField.value != "") {
+    if (playerNameField.value != "") {
         var playerName = playerNameField.value;
         console.log(playerName);
         let date = new Date();
-        let dateTime = date.getTime()+365 * 24 * 60 * 60 * 1000;
+        let dateTime = date.getTime() + 365 * 24 * 60 * 60 * 1000;
         setCookie("playerName", playerName, dateTime);
         console.log(getCookie("playerName"));
-        document.getElementById("form").remove();
+        document.getElementById("loginForm").remove();
         const urlParameters = new URLSearchParams(window.location.search);
         const gameid = urlParameters.get("uuid");
         startHunt(playerName, gameid);
     }
+    Rememberme(); // Call Rememberme function
+    return false; // Prevent form from submitting the traditional way
 }
+
+function Rememberme() {
+    const rememberMeCheckbox = document.getElementById("remember me");
+    if (rememberMeCheckbox.checked) {
+        const username = document.getElementById("fname").value;
+        setCookie("rememberedUser", username, 365);
+    } else {
+        deleteCookie("rememberedUser");
+    }
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 function startHunt(PlayerName,gameid)
 {
 
