7cc7fdc9be3c95349d50bd68fc7d3d084be84198
lowen <cofidea24@gmail.com>
2025-03-26 20:44:17 +0200

    Final update: added missing pages, styles, and logo updates

--- js/app.js
+++ js/app.js
@@ -1,226 +1,146 @@
-function getinfo(id) {
-    const playerNameField = document.getElementById("fname");
-    if(playerNameField.value != "") {
-        var playerName = playerNameField.value;
-        console.log(playerName);
-        let date = new Date();
-        let dateTime = date.getTime()+365 * 24 * 60 * 60 * 1000;
-        setCookie("playerName", playerName, dateTime);
-        console.log(getCookie("playerName"));
-        document.getElementById("form").remove();
-        const urlParameters = new URLSearchParams(window.location.search);
-        const gameid = urlParameters.get("uuid");
-        startHunt(playerName, gameid);
-    }
-}
-function startHunt(PlayerName,gameid)
-{
-
-    console.log(gameid);
-    var platform="TreasureHuntApp";
-    var URL = "https://codecyprus.org/th/api/start?player="+PlayerName+"&app="+platform+"&treasure-hunt-id="+gameid;
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-            console.log(jsonObject)
-            var sessionID = jsonObject.session;
-            var numOfQuestions = jsonObject.numOfQuestions;
-            getQuestion(sessionID);
-        });
-}
-function getQuestion(sessionID){
-    var URL = "https://codecyprus.org/th/api/question?session="+sessionID;
-    if(document.getElementById("secondWrap") != null)
-    {
-        document.getElementById("secondWrap").remove();
-    }
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-
-            j = document.getElementById("myWraper");
-            j.innerHTML += "<form id='form'>";
-            i = document.getElementById("form");
-            if(jsonObject.completed == false) {
-                if (jsonObject.questionType == "TEXT") {
-                    i.innerHTML += "<label for='ans'>" + jsonObject.questionText + "</label><br>" +
-                        "<input type='text' id='ans' name='ans'><br>" +
-                        "<input type='button' onclick='getAnswer(" + JSON.stringify(sessionID) + ", " + JSON.stringify(jsonObject.questionType) + ")' value='Submit'>";
-                    if (jsonObject.canBeSkipped) {
-                        i.innerHTML += "<input type='button' onclick='skipQuestion(" + JSON.stringify(sessionID) + ")' value='Skip the question'>";
-                    }
-                } else if (jsonObject.questionType == "NUMERIC" || jsonObject.questionType == "INTEGER") {
-                    if (jsonObject.questionType == "NUMERIC") {
-                        i.innerHTML += "<label for='ans'>" + jsonObject.questionText + "</label><br>" +
-                            "<input type='number' id='ans' name='ans' step='0.01'><br>" +
-                            "<input type='button' onclick='getAnswer(" + JSON.stringify(sessionID) + ", " + JSON.stringify(jsonObject.questionType) + ")' value='Submit'>";
-                        if (jsonObject.canBeSkipped) {
-                            i.innerHTML += "<input type='button' onclick='skipQuestion(" + JSON.stringify(sessionID) + ")' value='Skip the question'>";
-                        }
-                    } else {
-                        i.innerHTML += "<label for='ans'>" + jsonObject.questionText + "</label><br>" +
-                            "<input type='number' id='ans' name='ans'><br>" +
-                            "<input type='button' onclick='getAnswer(" + JSON.stringify(sessionID) + ", " + JSON.stringify(jsonObject.questionType) + ")' value='Submit'>";
-                        if (jsonObject.canBeSkipped) {
-                            i.innerHTML += "<input type='button' onclick='skipQuestion(" + JSON.stringify(sessionID) + ")' value='Skip the question'>";
-                        }
-                    }
-                } else {
-                    if (jsonObject.questionType == "BOOLEAN") {
-                        i.innerHTML += "<p>" + jsonObject.questionText + "</p>\n" +
-                            "<input type='radio' id='true' name='ans' value='True'>\n" +
-                            "<label for='true'>True</label><br>\n" +
-                            "<input type='radio' id='false' name='ans' value='False'>\n" +
-                            "<label for='false'>False</label><br>\n " +
-                            "<input type='button' onclick='getAnswer(" + JSON.stringify(sessionID) + ", " + JSON.stringify(jsonObject.questionType) + ")' value='Submit'>";
-                        if (jsonObject.canBeSkipped) {
-                            i.innerHTML += "<input type='button' onclick='skipQuestion(" + JSON.stringify(sessionID) + ")' value='Skip the question'>";
-                        }
-                    } else {
-                        i.innerHTML += "<p>" + jsonObject.questionText + "</p>\n" +
-                            "<input type='radio' id='A' name='ans' value='A'>\n" +
-                            "<label for='A'>A</label><br>\n" +
-                            "<input type='radio' id='B' name='ans' value='B'>\n" +
-                            "<label for='B'>B</label><br>\n " +
-                            "<input type='radio' id='C' name='ans' value='C'>\n" +
-                            "<label for='C'>C</label><br>\n" +
-                            "<input type='radio' id='D' name='ans' value='D'>\n" +
-                            "<label for='D'>D</label><br>\n " +
-                            "<input type='button' onclick='getAnswer(" + JSON.stringify(sessionID) + ", " + JSON.stringify(jsonObject.questionType) + ")' value='Submit'>";
-                        if (jsonObject.canBeSkipped) {
-                            i.innerHTML += "<input type='button' onclick='skipQuestion(" + JSON.stringify(sessionID) + ")' value='Skip the question'>";
-                        }
-                    }
-
-                }
-                j.innerHTML += "</form>";
-            }
-            else{
-                let param = new URLSearchParams(window.location.search);
-                j.innerHTML+="<div id='end'>" +
-                    "<h1 id='gameEnd'> Thanks for playing treasure hunt" +
-                    "<p>You can eather start new game or check your position on leaderboard</p>" +
-                    "<br>" +
-                    "<input type='button' onclick='getLeaderBoard(" + JSON.stringify(sessionID) + ")' value='Leader board'>" +
-                    "<input type='button' onclick='startAgain()' value='Start Again!'>" +
-                    "</div>"
-            }
-        });
-}
-function getAnswer(sessionID, QType){
-    console.log(sessionID);
-    if(QType == "NUMERIC" || QType == "INTEGER" || QType == "TEXT") {
-        var ans = document.getElementById("ans").value;
-    }
-    else{
-        var ans = document.querySelector('input[name="ans"]:checked').value;
-    }
-    var URL = "https://codecyprus.org/th/api/answer?session="+sessionID+"&answer="+ans;
-
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-            document.getElementById("form").remove();
-            var Congrats = jsonObject.message;
-            var scorAdjustment = jsonObject.scoreAdjustment;
-            i = document.getElementById("myWraper");
-            console.log(scorAdjustment);
-            console.log(Congrats);
-            i.innerHTML+="<div id='secondWrap'>" +
-                "<h1 id='congratulation'>"+Congrats+"</h1>" +
-                "<p id='scoreAdjustment'>You got +"+scorAdjustment+"</p>" +
-                "<input type='button' value='Continue' onclick='getQuestion("+JSON.stringify(sessionID)+")'>" +
-                "</div>";
-        });
-}
-function skipQuestion(sessionID){
-    var URL = "https://codecyprus.org/th/api/skip?session="+sessionID;
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-            var Congrats = jsonObject.message;
-            var scorAdjustment = jsonObject.scoreAdjustment;
-            document.getElementById("form").remove();
-            i = document.getElementById("myWraper");
-            i.innerHTML+="<div id='secondWrap'>" +
-                "<h1 id='congratulation'>"+Congrats+"</h1>" +
-                "<p id='scoreAdjustment'>You got "+scorAdjustment+"</p>" +
-                "<input type='button' value='Continue' onclick='getQuestion("+JSON.stringify(sessionID)+")'>" +
-                "</div>";
-        });
-}
-function getLocation(){
-    return navigator.geolocation.getCurrentPosition(showjPosition);
-}
-function getScore(sessionID){
-    var URL = "https://codecyprus.org/th/api/score?session="+sessionID;
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-            var Congrats = jsonObject.player;
-            var scorAdjustment = jsonObject.score;
-            i = document.getElementById("myWraper");
-            i+="<div id='secondWrap'>" +
-                "<h1 id='congratulation'>"+Congrats+"</h1>" +
-                "<p id='scoreAdjustment'>You got +"+scorAdjustment+"</p>" +
-                "<input type='button' value='Continue' onclick='getQuestion("+sessionID+")'>" +
-                "</div>";
-        });
-}
-
-function getLeaderBoard(sessionID) {
-    var URL = "https://codecyprus.org/th/api/leaderboard?session=" + sessionID + "&sorted";
-    fetch(URL)
-        .then(response => response.json()) //Parse JSON text to JavaScript object
-        .then(jsonObject => {
-            var players = jsonObject.leaderboard;
-            document.getElementById("end").remove();
-            i = document.getElementById("myWraper");
-            i.innerHTML+="<div id='secondWrap'>" +
-                "<h1>Your position in leaderboard is: "+findMinePosition(jsonObject, getCookie("playerName"))+"</h1>";
-            let k = document.getElementById("secondWrap");
-            for(let j = 0; j < 10; j++){
-                k.innerHTML+= "<h2>Player № "+(j+1)+" "+jsonObject.leaderboard[j].player+" and his score is: "+jsonObject.leaderboard[j].score+"</h2>";
-            }
-
-            i.innerHTML+="<input type='button' onclick='startAgain()' value='Start Again!'>" +
-                "</div>";
-        });
-}
-function findMinePosition(jsonObject, playerName){
-    for(let i = 0; i < jsonObject.numOfPlayers;i++)
-    {
-        if(JSON.stringify(jsonObject.leaderboard[i].player) === JSON.stringify(playerName))
-        {
-            return i+1;
-        }
+// Function to get the value of a specific cookie by name
+function getCookieValue(name) {
+    const cookies = document.cookie.split("; ");
+    for (const c of cookies) {
+      const [key, value] = c.split("=");
+      if (key === name) return decodeURIComponent(value); // will retorn the decoded cookie value if the name matches
     }
     return null;
-}
-function startAgain()
-{
-    location.replace("https://pgurulev.github.io/CO1111/Project/");
-}
-//Code from w3shools url="https://www.w3schools.com/js/js_cookies.asp"
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  }
+  
+  // cookie expiry date
+  function clearCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  }
+  
+  // check [layers name to avoild double entries
+  document.addEventListener("DOMContentLoaded", () => {
+    const playerName = getCookieValue("playerName");
+    const gameId = getCookieValue("uuid");
+    if (!playerName || !gameId) {
+      window.location.href = "start.html";
+      return;
     }
-    return "";
-}
-//www.w3schools.com/js/js_cookies.asp"
-function setCookie(cookieName, cookieValue, expireDays) {
-    let date = new Date();
-    date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-}
+  
+    startHunt(playerName, gameId);
+  });
+  //start game by calling  API
+  function startHunt(playerName, gameId) {
+    const appId = "Team D";
+    const startURL = `https://codecyprus.org/th/api/start?player=${encodeURIComponent(playerName)}&app=${encodeURIComponent(appId)}&treasure-hunt-id=${gameId}`;
+   
+    // request to start game session
+    fetch(startURL)
+      .then(res => res.json())
+      .then(json => {
+        if (json.status === "OK") {
+          getQuestion(json.session);
+        } else {
+          alert(json.errorMessages.join("\n"));
+          startAgain();
+        }
+      })
+      .catch(() => startAgain()); // If there a network error or any failure reset
+  }
+  // get and display the current question for player
+  function getQuestion(sessionID) {
+    fetch(`https://codecyprus.org/th/api/question?session=${sessionID}`)
+      .then(res => res.json())
+      .then(json => {
+        const wrapper = document.getElementById("myWraper");
+        wrapper.innerHTML = ""; // Clear previous content
+  
+        if (!json.completed) {    //  populate form with  current question
+          const form = document.createElement("form");
+          form.id = "form";
+          form.innerHTML = `<label>${json.questionText}</label><br>`;
+  
+          switch (json.questionType) {
+            case "TEXT":
+              form.innerHTML += `<input type="text" id="ans"><br>`;
+              break;
+            case "NUMERIC":
+            case "INTEGER":
+              form.innerHTML += `<input type="number" id="ans" step="0.01"><br>`;
+              break;
+            case "BOOLEAN":
+              form.innerHTML += `
+                <input type="radio" name="ans" value="True">True<br>
+                <input type="radio" name="ans" value="False">False<br>`;
+              break;
+            case "MCQ":
+              ["A", "B", "C", "D"].forEach(opt => {
+                form.innerHTML += `<input type="radio" name="ans" value="${opt}">${opt}<br>`;
+              });
+              break;
+          }
+          // submit button
+          form.innerHTML += `<button type="button" onclick="getAnswer('${sessionID}', '${json.questionType}')">Submit</button>`;
+  
+          if (json.canBeSkipped) {
+            form.innerHTML += `<button type="button" onclick="skipQuestion('${sessionID}')">Skip</button>`;
+          }
+  
+          wrapper.appendChild(form);
+        } else {
+             // If quiz is finish, show thankx message and options of start again and leaderboa
+          wrapper.innerHTML = `
+            <div id='end'>
+              <h1>Thanks for playing the Treasure Hunt!</h1>
+              <p>You can start a new game or check the leaderboard.</p>
+              <button onclick="getLeaderBoard('${sessionID}')">Leaderboard</button>
+              <button onclick="startAgain()">Start Again</button>
+            </div>`;
+        }
+      });
+  }
+//   handle input
+  function getAnswer(sessionID, QType) {
+    let ans;
+    if (["NUMERIC", "INTEGER", "TEXT"].includes(QType)) {
+      ans = document.getElementById("ans").value.trim();
+      if (!ans) return alert("Please provide your answer.");
+    } else {
+      const selected = document.querySelector('input[name="ans"]:checked');
+      if (!selected) return alert("Please select an answer!");
+      ans = selected.value;
+    }
+//   Submit the answer to the API
+    fetch(`https://codecyprus.org/th/api/answer?session=${sessionID}&answer=${encodeURIComponent(ans)}`)
+      .then(res => res.json())
+      .then(json => {
+        document.getElementById("form").remove();
+        const wrapper = document.getElementById("myWraper");
+        wrapper.innerHTML += `<div id='secondWrap'><h1>${json.message}</h1><p>You got ${json.scoreAdjustment} points</p><button onclick="getQuestion('${sessionID}')">Continue</button></div>`;
+      });
+  }
+  // skipping a question
+  function skipQuestion(sessionID) {
+    fetch(`https://codecyprus.org/th/api/skip?session=${sessionID}`)
+      .then(res => res.json())
+      .then(json => {
+        document.getElementById("form").remove();
+        const wrapper = document.getElementById("myWraper");
+        wrapper.innerHTML += `<div id='secondWrap'><h1>${json.message}</h1><p>You got ${json.scoreAdjustment} points</p><button onclick="getQuestion('${sessionID}')">Continue</button></div>`;
+      });
+  }
+  //display the leaderboard after the game
+  function getLeaderBoard(sessionID) {
+    fetch(`https://codecyprus.org/th/api/leaderboard?session=${sessionID}&sorted`)
+      .then(res => res.json())
+      .then(json => {
+        const wrapper = document.getElementById("myWraper");
+        wrapper.innerHTML = `<div id='secondWrap'><h1>Leaderboard</h1>`;
+        // Display top 20 scores
+        json.leaderboard.slice(0, 20).forEach((player, index) => {
+          wrapper.innerHTML += `<h2>${index + 1}. ${player.player} - ${player.score} points</h2>`;
+        });
+        wrapper.innerHTML += `<button onclick="startAgain()">Start Again</button></div>`;
+      });
+  }
+  
+  function startAgain() {
+    clearCookie("playerName");
+    clearCookie("uuid");
+    location.replace("index.html");
+  }
